f2ec07433606c90859cea8a74081b2ae1bf84837
Tommy Arbuthnot <tommyarbuthnot@yahoo.COM>
2018-03-21 12:06:49 -0500

    tuesday's homework

---
+++ loops.js
@@ -0,0 +1,33 @@
+for(i = 0; i <= 99; i++) {
+  console.log(i);
+}
+
+for (var i = 99; i >= 0;  i--) {
+  console.log(i);
+}
+
+for (var i = 0; i <= 98; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+for (var i = 98; i >= 0; i--) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+for(i = 49; i <= 72; i++) {
+  console.log(i);
+}
+
+for (var i = 81; i >= 26;  i--) {
+  console.log(i);
+}
+
+for (var i = 3; i <= 90; i++) {
+  if (i % 3 === 0) {
+    console.log(i);
+  }
+}
